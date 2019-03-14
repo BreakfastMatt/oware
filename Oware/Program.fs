b147314453fa694e1242034600d6fce131523edb
Matthew Lewis <G17L3581@ict.ru.ac.za>
2019-03-14 17:29:19 +0200

    Made changes to the useHouse function and added theChosenHouse and incrementHouseSeed helper functions (to help implement useHouse)

--- Oware/Program.fs
+++ Oware/Program.fs
@@ -34,7 +34,48 @@ let getSeeds n board =
     |12 -> f'
     |_  -> failwith "Invalid choice of house"
 
-let useHouse n board = failwith "Not implemented b"
+let theChosenHouse n (a,b,c,d,e,f,a',b',c',d',e',f') = function
+    |1  -> (0,b,c,d,e,f,a',b',c',d',e',f') 
+    |2  -> (a,0,c,d,e,f,a',b',c',d',e',f') 
+    |3  -> (a,b,0,d,e,f,a',b',c',d',e',f') 
+    |4  -> (a,b,c,0,e,f,a',b',c',d',e',f')
+    |5  -> (a,b,c,d,0,f,a',b',c',d',e',f') 
+    |6  -> (a,b,c,d,e,0,a',b',c',d',e',f') 
+    |7  -> (a,b,c,d,e,f,0,b',c',d',e',f') 
+    |8  -> (a,b,c,d,e,f,a',0,c',d',e',f') 
+    |9  -> (a,b,c,d,e,f,a',b',0,d',e',f') 
+    |10 -> (a,b,c,d,e,f,a',b',c',0,e',f') 
+    |11 -> (a,b,c,d,e,f,a',b',c',d',0,f') 
+    |12 -> (a,b,c,d,e,f,a',b',c',d',e',0) 
+    |_  -> failwith "Someone done did a fuck up"
+
+let incrementHouseSeed n (a,b,c,d,e,f,a',b',c',d',e',f') = 
+    let n = match n with
+            |13 -> 1
+            |_ -> n
+    match n with 
+    |1 -> (a+1,b,c,d,e,f,a',b',c',d',e',f')
+    |2 -> (a,b+1,c,d,e,f,a',b',c',d',e',f')
+    |3 -> (a,b,c+1,d,e,f,a',b',c',d',e',f')
+    |4 -> (a,b,c,d+1,e,f,a',b',c',d',e',f')
+    |5 -> (a,b,c,d,e+1,f,a',b',c',d',e',f')
+    |6 -> (a,b,c,d,e,f+1,a',b',c',d',e',f')
+    |7 -> (a,b,c,d,e,f,a'+1,b',c',d',e',f')
+    |8 -> (a,b,c,d,e,f,a',b'+1,c',d',e',f')
+    |9 -> (a,b,c,d,e,f,a',b',c'+1,d',e',f')
+    |10 -> (a,b,c,d,e,f,a',b',c',d'+1,e',f')
+    |11 -> (a,b,c,d,e,f,a',b',c',d',e'+1,f')
+    |12 -> (a,b,c,d,e,f,a',b',c',d',e',f'+1)
+    |_ -> failwith "Sad cause bad"
+    
+let useHouse n board =
+    let (a,b,c,d,e,f),(a',b',c',d',e',f') = board.playerOne.houses,board.playerTwo.houses 
+    let updatedHouses = (a,b,c,d,e,f,a',b',c',d',e',f') 
+    let updatedHouses = theChosenHouse n updatedHouses
+
+    let numSeeds = getSeeds n board
+    //Implement recursive function to distribute seeds from selected house to other houses
+    failwith "{{{{{{{{{{Not finished yet}}}}}}}}}}"
 
 let start position =
     let h = (4,4,4,4,4,4)
