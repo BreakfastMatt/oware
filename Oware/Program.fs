1f0df8c5924956dc62bb4fa7e99708af366e92fa
Damon Wolff <G17W6029@ict.ru.ac.za>
2019-03-14 15:28:44 +0200

    getSeeds function started

--- Oware/Program.fs
+++ Oware/Program.fs
@@ -4,7 +4,23 @@ type StartingPosition =
     | South
     | North
 
-let getSeeds n board = failwith "Not implemented"
+let getSeeds n  board = 
+    let board.Houses = (a,b,c,d,e,f,a',b',c',d',e',f')
+    match n with 
+    |1 -> a
+    |2 -> b
+    |3 -> c
+    |4 -> d
+    |5 -> e
+    |6 -> f
+    |7 -> a'
+    |8 -> b'    
+    |9 -> c'
+    |10 -> d'
+    |11 -> e'
+    |12 -> f'
+    |_  -> -1
+
 
 let useHouse n board = failwith "Not implemented"
 
