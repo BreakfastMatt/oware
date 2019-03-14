0b59561f63b0b3d23eefbc349f6c877a476d09fd
Makungu Chansa <G17C9313@ict.ru.ac.za>
2019-03-14 15:33:17 +0200

    working on Game state

--- Oware/Program.fs
+++ Oware/Program.fs
@@ -12,7 +12,24 @@ let start position = failwith "Not implemented"
 
 let score board = failwith "Not implemented"
 
-let gameState board = failwith "Not implemented"
+let gameState board = 
+   let a,b,c,d,e,f,g,h,i,j,k,l = board.Houses
+   let x,y = board.Scores 
+   match x > (y + (a + b + c + d + e + f + g + h + i + j + k + l))  with 
+   |true -> "South wins"
+   |false -> 
+        match x > (y + (a + b + c + d + e + f + g + h + i + j + k + l))  with 
+        |true -> "South wins"
+        |false -> 
+   match board.CurrentTurn with
+   |South -> "South's turn"
+   |North -> "North's turn"
+   
+   (*match board with 
+   |(0,0,0,0,0,0,0,0,0,0,0,0),_,_,_ ->  "score board" 
+   |_,_,_,"South's turn" -> "North's turn"
+   |_,_,_, "North's turn" -> "South's turn"
+   //failwith "Not implemented"*)
 
 [<EntryPoint>]
 let main _ =
